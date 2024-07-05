8870c9b4f0ebbd27f9c0d5ca471ca57cde566fb7
NozoDev <kevinroj326@gmail.com>
2024-07-04 19:25:04 -0500

    haciendo el navbar estilos

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 export const Search = () => {
   return (
-    <form>
-      <div className="flex relative items-center">
+    <form className="">
+      <div className="flex relative items-center cursor-pointer">
         <input
-          className="pl-10 pr-4 py-2 border rounded"
+          className="pl-10 pr-4 py-2 border rounded cursor-pointer"
           type="text"
           placeholder="What are you loocking for?"
         />{" "}
         <i className="bx bx-search absolute left-3 text-gray-300"></i>
-        <div className="flex p-2 gap-1">
-          <i className="bx bx-heart text-xl"></i>
+        <div className="flex p-2 gap-4">
+          <i className="bx bx-heart text-xl animate-bounce hover:text-red-400"></i>
           <i class="bx bx-cart text-xl"></i>
         </div>
       </div>

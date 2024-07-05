5157c0c954de4970d49540d76e0f6f304de643f5
NozoDev <kevinroj326@gmail.com>
2024-07-04 19:21:31 -0500

    aplicando estilos al navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Search } from "../pages/Search";
 
 export const Navbar = () => {
   return (
-    <nav className="flex bg-red-200 items-center">
+    <nav className="flex bg-cyan-200 bg-opacity-50 items-center p-4 px-10">
       <div>
-        <h1> Exclusive</h1>
+        <h1 className="text-2xl font-semibold"> Exclusive</h1>
       </div>
-      <div className="text-center flex justify-center w-full items-center">
-        <Link to="/">Home</Link>
-        <Link to="/contact">contact</Link>
-        <Link to="/about">About</Link>
-        <Link to="/singup">Sign Up</Link>
+      <div className="text-center flex justify-center w-full items-center gap-4">
+        <Link
+          className="hover:animate-pulse hover:text-opacity-10 hover:border-b-4 border-indigo-500"
+          to="/"
+        >
+          Home
+        </Link>
+        <Link
+          className="hover:animate-pulse hover:text-opacity-10 hover:border-b-4 border-indigo-500"
+          to="/contact"
+        >
+          contact
+        </Link>
+        <Link
+          className="hover:animate-pulse hover:text-opacity-10 hover:border-b-4 border-indigo-500"
+          to="/about"
+        >
+          About
+        </Link>
+        <Link
+          className="hover:animate-pulse hover:text-opacity-10 hover:border-b-4 border-indigo-500"
+          to="/singup"
+        >
+          Sign Up
+        </Link>
       </div>
+      <Search />
     </nav>
   );
 };
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <form>
+      <div className="flex relative items-center">
+        <input
+          className="pl-10 pr-4 py-2 border rounded"
+          type="text"
+          placeholder="What are you loocking for?"
+        />{" "}
+        <i className="bx bx-search absolute left-3 text-gray-300"></i>
+        <div className="flex p-2 gap-1">
+          <i className="bx bx-heart text-xl"></i>
+          <i class="bx bx-cart text-xl"></i>
+        </div>
+      </div>
+    </form>
+  );
+};
